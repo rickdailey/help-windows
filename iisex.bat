2d0f1865379c358ab5db7dd0c8f7e7672f84cf5c
rickdailey <rd@rckd.ly>
2014-03-07 15:00:48 -0800

    Fixed to read configuration from registry for install directory

--- iisex.bat
+++ iisex.bat
@@ -1,6 +1,15 @@
 SET PortNumber="%1"
 IF [%1] == [] SET PortNumber=8080
 
-start "IIS Express" "C:\Program Files (x86)\IIS Express\iisexpress.exe" /path:%CD% /port:%PortNumber%
+SET InstallDir="C:\Program Files (x86)\IIS Express\"
+
+SET RegKey=HKLM\Software\Microsoft\IISExpress
+SET RegVal=InstallPath
+for /f "tokens=2,*" %%a in ('reg query %RegKey% /s /v %RegVal% ^| findstr %RegVal%') do (
+    SET InstallDir=%%b
+)
+
+ECHO %InstallDir%
+
+start "IIS Express" "%InstallDir%\iisexpress.exe" /path:"%CD%" /port:%PortNumber%
 start "Browser" "http://localhost:%PortNumber%"
-pause
